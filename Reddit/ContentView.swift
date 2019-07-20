00a6201ee49e53beb4e8f9bad3f67410fa6524f3
Carson Katri <carson.katri@gmail.com>
2019-07-20 08:25:57 -0400

    Initial Commit

---
+++ Reddit/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Reddit
+//
+//  Created by Carson Katri on 7/20/19.
+//  Copyright © 2019 Carson Katri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
